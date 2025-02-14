076e71fc5562b9a3e835f35722080af1bd9cb762
Programlearner1 <113779810+programlearner1@users.noreply.github.com>
2025-02-14 09:46:16 +0530

    deployment errors

--- music/server.js
+++ music/server.js
@@ -10,6 +10,7 @@ const YOUTUBE_API_KEY = "AIzaSyB8Phtr6Jlm9wRm-Ja51QhNZiA1zM4541o"; // Replace wi
 const app = express();
 app.use(cors());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static("public")); // Serve files from /public
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
